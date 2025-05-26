32b9a92c3fbaffd43afa73ab4b6c6cc29be99367
ginoasuncion <ginoasuncion@gmail.com>
2025-05-26 17:59:47 +0700

    Add unit test for SimpleFactory

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "testing"
+)
 
-func main() {
-    fmt.Println("Hello from myapp!")
+type Simple struct {
+    Greeting string
+    Subject  string
+    Host     string
 }
 
+func SimpleFactory(host string) Simple {
+    return Simple{"Hello", "World", host}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    simple := SimpleFactory(r.Host)
+    fmt.Fprintf(w, "%v", simple)
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    fmt.Println("Starting server at http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
+}
