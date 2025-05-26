a52f15b589be08119bd7648472cb807739bb97f9
ginoasuncion <ginoasuncion@gmail.com>
2025-05-26 21:07:14 +0700

    Update main.go to return JSON from HTTP server

--- main.go
+++ main.go
@@ -2,17 +2,28 @@ package main
 
 import (
     "encoding/json"
+    "fmt"
     "net/http"
 )
 
-type Message struct {
-    Message string `json:"message"`
+type Simple struct {
+    Greeting string `json:"greeting"`
+    Subject  string `json:"subject"`
+    Host     string `json:"host"`
+}
+
+func SimpleFactory(host string) Simple {
+    return Simple{"Hello", "World", host}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    simple := SimpleFactory(r.Host)
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(simple)
 }
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        json.NewEncoder(w).Encode(Message{Message: "Hello from myapp!"})
-    })
-    http.ListenAndServe(":80", nil)
+    http.HandleFunc("/", handler)
+    fmt.Println("Starting server at http://localhost:8080")
+    http.ListenAndServe(":8080", nil)
 }
-
